3fa8c19c24d0beabd1b0bd9388ce2e50d5922e2b
muschellij2 <muschellij2@gmail.com>
2025-12-31 15:44:24 -0500

    fixing test

--- tests/testthat/test-translate_download.R
+++ tests/testthat/test-translate_download.R
@@ -1,15 +1,20 @@
-test_that("nhanesTranslate: downloading a file works", {
+nhanesA::nhanesOptions(log.access = TRUE)
+testthat::test_that("nhanesTranslate: downloading a file works", {
   testthat::skip_if_offline()
   tfile = tempfile(fileext = ".htm")
   res = nhanesA::nhanesTranslate("DEMO_G", file = tfile)
   testthat::expect_true(file.exists(tfile))
-  res2 = nhanesA::nhanesTranslate("DEMO_G", file = tfile)
+  testthat::expect_message({
+    res2 = nhanesA::nhanesTranslate("DEMO_G", file = tfile)
+  }, regexp = paste0("Downloading.*", basename(tfile)))
 })
 
-test_that("nhanesTranslate: using an existing file works", {
+testthat::test_that("nhanesTranslate: using an existing file works", {
   test_file = system.file("DEMO_G.htm", package = "nhanesA")
   # give garbage table but still read the file
-  res = nhanesA::nhanesTranslate("asdf", file = tfile)
+  testthat::expect_message({
+    res = nhanesA::nhanesTranslate("asdf", file = test_file)
+  }, regexp = paste0("Downloading.*", basename(test_file)))
   testthat::expect_named(
     res,
     c("SDDSRVYR", "RIDSTATR", "RIAGENDR", "RIDAGEYR", "RIDAGEMN", 
